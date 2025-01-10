1dbbab12140068cff6b50d603c2d1ee8fae4726d
Матвей Доровин <mwpaa@yandex.ru>
2025-01-11 00:04:41 +0500

    add a package

---
+++ mymath/mymath.go
@@ -0,0 +1,22 @@
+package mymath
+
+import "math"
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+func Ceil(num float64) float64 {
+	return math.Ceil(num)
+}
+func Floor(num float64) float64 {
+	return math.Floor(num)
+}
+func Pow(num, po float64) float64 {
+	return math.Pow(num, po)
+}
+func Max(ma, mi float64) float64 {
+	return math.Max(ma, mi)
+}
+func Min(ma, mi float64) float64 {
+	return math.Min(ma, mi)
+}
